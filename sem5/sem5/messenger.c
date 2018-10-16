00b1f52156648d8ea055e4a5d7a3cc5ed4afce65
AlinaKayd <kajdash.aa@phystech.edu>
2018-10-16 21:25:37 +0300

    final messenger

--- sem5/sem5/messenger.c
+++ sem5/sem5/messenger.c
@@ -49,20 +49,6 @@ int main(int argc, char **argv){
         exit(-1);
     }
     if(result > 0) {
-        if(atoi(argv[1]) == 2){
-            if((fd2 = open(name2, O_WRONLY)) < 0){
-                printf("Can\'t open FIFO for writting\n");
-                exit(-1);
-            }
-            while(1)
-            {
-                char * str1 = (char*)malloc(maxstr*sizeof(char));
-                fgets(str1, maxstr, stdin);
-                write(fd2, str1, strlen(str1));
-                free(str1);
-                
-            }
-        }
         if(atoi(argv[1]) == 1){
             if((fd1 = open(name1, O_WRONLY)) < 0){
                 printf("Can\'t open FIFO for writting\n");
@@ -70,37 +56,52 @@ int main(int argc, char **argv){
             }
             while(1)
             {
-                char * str1 = (char*)malloc(maxstr*sizeof(char));
+                char * str1 = (char*)calloc(maxstr, sizeof(char));
                 fgets(str1, maxstr, stdin);
-                //str1[strlen(str1) + 1] = '\0';
+                str1[strlen(str1) + 1] = '\0';
+                //printf("%s", str1);
                 write(fd1, str1, strlen(str1));
                 free(str1);
+            }
+        }
+        if(atoi(argv[1]) == 2){
+            if((fd2 = open(name2, O_WRONLY)) < 0){
+                printf("Can\'t open FIFO for writting\n");
+                exit(-1);
+            }
+            while(1)
+            {
+                char * str1 = (char*)calloc(maxstr, sizeof(char));
+                fgets(str1, maxstr, stdin);
+                write(fd2, str1, strlen(str1));
+                free(str1);
+                
             }
         }
        // printf("read fd = %d\n", fd);
     }
     if(result == 0) {
-        if(atoi(argv[1]) == 2) {
-            if((fd1 = open(name1, O_RDONLY)) < 0){
-                printf("Can\'t open FIFO for reading\n");
-                exit(-1);
-            }
-            while(1) {
-                char * str2 = (char*)malloc(maxstr*sizeof(char));
-                read(fd1, str2, maxstr);
-                //str2[strlen(str2)+1] = '\0';
-                printf("%s", str2);
-                free(str2);
-            }
-        }
         if(atoi(argv[1]) == 1) {
             if((fd2 = open(name2, O_RDONLY)) < 0){
                 printf("Can\'t open FIFO for reading\n");
                 exit(-1);
             }
             while(1) {
-                char * str2 = (char*)malloc(maxstr*sizeof(char));
+                char * str2 = (char*)calloc(maxstr, sizeof(char));
                 read(fd2, str2, maxstr);
+                //str2[strlen(str2)+1] = '\0';
+                printf("%s", str2);
+                free(str2);
+            }
+        }
+        if(atoi(argv[1]) == 2) {
+            if((fd1 = open(name1, O_RDONLY)) < 0){
+                printf("Can\'t open FIFO for reading\n");
+                exit(-1);
+            }
+            while(1) {
+                char * str2 = (char*)calloc(maxstr, sizeof(char));
+                read(fd1, str2, maxstr);
                 //str2[strlen(str2)+1] = '\0';
                 printf("%s", str2);
                 free(str2);
